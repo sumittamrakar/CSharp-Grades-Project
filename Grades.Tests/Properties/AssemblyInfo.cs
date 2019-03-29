9e5f98e15789db094b540ef30c4a362778cd8cb1
Sumit Tamrakar <tamrakarSumit7@gmail.com>
2019-03-28 23:06:18 -0600

    First Commit

---
+++ Grades.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Grades.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Grades.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("bcf8582c-e6bf-4dd2-8070-1d1197a1290e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Grades.Tests/Types/ReferenceTypeTests.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grades.Tests.Types
+{
+    [TestClass]
+    public class TypeTests
+    {
+        [TestMethod]
+        public void ValueTypePassByValue()
+        {
+            int x = 46;
+            IncrementNumber(x);
+
+            Assert.AreEqual(46, x);
+        }
+
+        private void IncrementNumber(int number)
+        {
+            number += 1;
+        }
+
+        [TestMethod]
+        public void ReferenceTypePassByValue()
+        {
+            GradeBook book1 = new GradeBook();
+            GradeBook book2 = book1;
+
+            GiveBookAName(book2);
+
+            Assert.AreEqual("A GradeBook", book1.name);
+
+        }
+
+        private void GiveBookAName(GradeBook book)
+        {
+            book.name = "A GradeBook";
+
+        }
+
+        [TestMethod]
+        public void StringComparisons()
+        {
+            string name1 = "Sumit";
+            string name2 = "sumit";
+
+            bool result = string.Equals(name1, name2, StringComparison.InvariantCultureIgnoreCase);
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void IntVariablesHoldValues()
+        {
+            int x1 = 100;
+            int x2 = x1;
+
+            x1 = 4;
+            Assert.AreNotEqual(x1, x2);
+        }
+
+        [TestMethod]
+        public void GradeBookVariablesHoldReference()
+        {
+            GradeBook g1 = new GradeBook();
+            GradeBook g2 = g1;
+
+            g1 = new GradeBook();
+            g1.name = "Sumit's grade book";
+
+            Assert.AreNotEqual(g1.name, g2.name);
+        }
+    }
+}
